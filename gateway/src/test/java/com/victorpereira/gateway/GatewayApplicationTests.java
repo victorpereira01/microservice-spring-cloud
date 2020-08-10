7272a1a1ce90b62dfdda6d721f4ec09c43b54a5a
Victor Pereira <victor.cordeiro2@gmail.com>
2020-08-09 23:03:58 -0300

    gateway

---
+++ gateway/src/test/java/com/victorpereira/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorpereira.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
